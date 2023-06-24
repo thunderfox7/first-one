61fc0e94aad6a5de7abaa73f5280b9dbd2bfd76a
Alan Rodríguez <alan.rodgar8@gmail.com>
2023-06-24 11:29:57 -0600

    .

--- ejercicio-2.js
+++ ejercicio-2.js
@@ -1,1 +1,35 @@
 console.log('Hi, im a placeholder');
+function makeCalendar () {
+    let selectedTimeZone = 'America/Mexico_City'
+    let now = new Date();
+    let localNow = new Date(now.toLocaleString('en-US', { timeZone: selectedTimeZone }))
+    let day = localNow.toLocaleString('es-MX', { weekday:'long' })
+    day = day[0].toUpperCase() + day.slice(1)
+    let date = localNow.getDate()
+    let month = localNow.toLocaleString('es-MX', { month:'long' })
+    month = month[0].toUpperCase() + month.slice(1)
+    let year = localNow.getFullYear()
+    calendarBody.textContent = day + ' ' + date + ' de ' + month + ' de ' + year
+
+    let hours = localNow.getHours()
+    let pm = false
+    if (hours > 12) {
+        hours = hours - 12
+        pm = true
+    } else {
+        if (hours === 0) hours = 12
+    }
+    let minutes = localNow.getMinutes()
+    if (minutes < 10) minutes = '0' + minutes
+    let seconds = localNow.getSeconds()
+    if (seconds < 10) seconds = '0' + seconds
+
+    document.querySelector('#hours').textContent = hours
+    document.querySelector('#mins').textContent = minutes
+    document.querySelector('#seconds').textContent = seconds
+    
+    const timeType = pm ? 'pm' : 'am';
+    document.querySelector('#type').textContent = timeType;
+}
+
+let clock = setInterval(makeCalendar, 1000);
